03640879fdcd3a4d9401fcdcd992dc9c6de33707
IffyBog <aatony018@protonmail.com>
2025-04-16 22:54:47 -0400

    Set location is now functional, displays weather info, and is persistent till next page visit.

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,12 +1,13 @@
 
-// Replace with form input later 
-const city = "Harrisonburg";
-const locality = "Virginia";
+// Default location
+let city = "Harrisonburg";
+let locality = "Virginia";
 const tempUnit = "fahrenheit";
 
 // Given user input, we find the temperature for the user's city
 let latitude, longitude;
-fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+const getWeather = (city, locality) => {
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
     .then(response => response.json())
     .then((data) => {
         // We're using .find() because it stops at the first city that matches the location
@@ -28,6 +29,8 @@ fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&lang
                 temperature = data.current.temperature_2m;
                 unit = data.current_units.temperature_2m;
                 console.log(`The current temperature in ${city} ${locality} is ${temperature}${unit}`)
+                weatherNode = document.getElementById("weather");
+                weatherNode.innerHTML = `Temp: ${temperature}${unit} -  ${city}, ${locality}` 
             })
             .catch((error) => {
                 throw new Error("Error while obtaining temperature data: " + error)
@@ -36,3 +39,24 @@ fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&lang
     .catch((error) => {
         throw new Error("Error while obtaining weather information: " + error);
     })
+}
+
+// On page load
+if (!localStorage.getItem("city")) {
+    getWeather(city, locality);
+} else {
+    city = localStorage.getItem("city");
+    locality = localStorage.getItem("locality");
+    getWeather(city, locality);
+}
+
+// Call for updated values
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener('click', () => {
+    city = document.getElementById("city").value;
+    locality = document.getElementById("locality").value;
+    // Add data to localStorage
+    localStorage.setItem("city", city);
+    localStorage.setItem("locality", locality);
+    getWeather(city, locality);
+});
