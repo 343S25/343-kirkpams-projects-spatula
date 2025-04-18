408dd89ec443a265544326f069f2b8dabdf84bda
eshanker <elianashanker@gmail.com>
2025-04-18 15:36:18 -0400

    add fallback date picker, update some variable names

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,94 +1,126 @@
-const month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let current_date = new Date();
+let currentDate = new Date();
 const weeks = document.getElementById("calendar-boxes").getElementsByClassName("row");
 
-function buildCalendar(date) {
-    let base_month = date.getMonth();
-    let base_day = date.getDay();
+function isLeapYear(year) {
+    return (year % 4 == 0) && ((year % 400 == 0) || (year % 100 != 0));
+}
 
-    let curr_date = 1;
-    let curr_week = 1;
-    let curr_day = 0;
+function buildCalendar(date) {
+    let baseMonth = date.getMonth();
+    let baseDay = date.getDay();
+
+    let currDate = 1;
+    let currWeek = 1;
+    let currDay = 0;
 
     // this fills in the days before the start of the month by starting on Sunday and going until it hits the first day of the month
-    while (curr_day < date.getDay()) {
-        let days = weeks[curr_week].getElementsByClassName("calendar-day");
-        days[curr_day].textContent = month_lengths[(base_month + 11) % 12] + (curr_day - base_day) + 1;
-        days[curr_day].classList = "col calendar-day other-month";
-        curr_day++;
+    while (currDay < date.getDay()) {
+        let days = weeks[currWeek].getElementsByClassName("calendar-day");
+        days[currDay].textContent = monthLengths[(baseMonth + 11) % 12] + (currDay - baseDay) + 1;
+        days[currDay].classList = "col calendar-day other-month";
+        currDay++;
     }
 
     // this fills in the days of the current month
-    while (curr_date < month_lengths[base_month]) {
-        let days = weeks[curr_week].getElementsByClassName("calendar-day");
+    while (currDate < monthLengths[baseMonth]) {
+        let days = weeks[currWeek].getElementsByClassName("calendar-day");
 
         // inner loop handles each individual
-        while (curr_day < 7 && curr_date <= month_lengths[base_month]) {
-            days[curr_day].textContent = curr_date;
-            days[curr_day].classList = "col calendar-day curr-month";
+        while (currDay < 7 && currDate <= monthLengths[baseMonth]) {
+            days[currDay].textContent = currDate;
+            days[currDay].classList = "col calendar-day curr-month";
 
-            if (curr_date == current_date.getDate() && baseDate.getMonth() == current_date.getMonth() && baseDate.getFullYear() == current_date.getFullYear()) {
-                days[curr_day].classList.add("curr-day");
+            if (currDate == currentDate.getDate() && baseDate.getMonth() == currentDate.getMonth() && baseDate.getFullYear() == currentDate.getFullYear()) {
+                days[currDay].classList.add("curr-day");
             }
-            curr_date++;
-            curr_day++;
+            currDate++;
+            currDay++;
         }
 
-        if (curr_day == 7) {
-            curr_week++;
-            curr_day = 0;
+        if (currDay == 7) {
+            currWeek++;
+            currDay = 0;
         }
     }
 
     // this fills in the days after the end of the current month. pretty similar to the last one as it just goes until there aren't any more weeks
-    curr_date = 1;
-    while (curr_week <= 6) {
-        let days = weeks[curr_week].getElementsByClassName("calendar-day");
+    currDate = 1;
+    while (currWeek <= 6) {
+        let days = weeks[currWeek].getElementsByClassName("calendar-day");
 
-        while (curr_day < 7 && curr_week <= 6) {
-            days[curr_day].textContent = curr_date;
-            days[curr_day].classList = "col calendar-day other-month";
-            curr_date++;
-            curr_day++;
+        while (currDay < 7 && currWeek <= 6) {
+            days[currDay].textContent = currDate;
+            days[currDay].classList = "col calendar-day other-month";
+            currDate++;
+            currDay++;
         }
 
-        curr_week++;
-        curr_day = 0;
+        currWeek++;
+        currDay = 0;
 
     }
 }
 
-let baseDate = new Date(current_date.getFullYear(), current_date.getMonth(), 1);
+let baseDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
 
 const leftButton = document.getElementById("calendar-button-left");
 const rightButton = document.getElementById("calendar-button-right");
 const jumpButton = document.getElementById("date-submit");
-const dateSelect = document.getElementById("date-input");
+
+const defaultDateSelect = document.getElementById("date-input-default");
+const fallbackDateSelect = document.getElementById("date-input-fallback");
+
+// by default, hide fallback date selector
+fallbackDateSelect.style.display = "none";
+
+// attempt to create a month input to figure out if the current browser supports it
+const test = document.createElement("input");
+try {
+    test.type = "month";
+} catch (e) { }
+
+if (test.type === "text") {
+    defaultDateSelect.style.display = "none";
+    fallbackDateSelect.style.display = "block";
+}
 
 leftButton.addEventListener("click", function () {
     baseDate.setMonth(baseDate.getMonth() - 1);
-    dateSelect.value = baseDate.toISOString().substring(0, 7);
+    defaultDateSelect.value = baseDate.toISOString().substring(0, 7);
+    fallbackDateSelect.value = baseDate.toISOString().substring(0, 10);
+    monthLengths[1] = isLeapYear(baseDate.getFullYear()) ? 29 : 28;
     buildCalendar(baseDate);
 });
 
 rightButton.addEventListener("click", function () {
     baseDate.setMonth(baseDate.getMonth() + 1);
-    dateSelect.value = baseDate.toISOString().substring(0, 7);
+    defaultDateSelect.value = baseDate.toISOString().substring(0, 7);
+    fallbackDateSelect.value = baseDate.toISOString().substring(0, 10);
+    monthLengths[1] = isLeapYear(baseDate.getFullYear()) ? 29 : 28;
     buildCalendar(baseDate);
 });
 
+
 jumpButton.addEventListener("click", function (event) {
-    let target = dateSelect.value.split("-");
+    let target;
+    if (test.type === "text") {
+        target = fallbackDateSelect.value.split("-");
+    } else {
+        target = defaultDateSelect.value.split("-");
+    }
     baseDate.setYear(parseInt(target[0]));
     baseDate.setMonth(parseInt(target[1]) - 1);
+    monthLengths[1] = isLeapYear(baseDate.getFullYear()) ? 29 : 28;
 
     buildCalendar(baseDate);
     event.preventDefault();
 });
 
-dateSelect.value = baseDate.toISOString().substring(0, 7);
+defaultDateSelect.value = baseDate.toISOString().substring(0, 7);
+fallbackDateSelect.value = currentDate.toISOString().substring(0, 10);
 
 buildCalendar(baseDate);
