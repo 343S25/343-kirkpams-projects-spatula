1bee4c865963da4b0b486ea8bd4bf4fe5123db3d
IffyBog <aatony018@protonmail.com>
2025-05-02 14:23:25 -0400

    Update header(s)

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,13 +1,6 @@
 // Main js file for the header
 
 // Sets tasks button to switch to task settings page
-document.getElementById('tasks').onclick = () => {
-    if (window.location.href.slice(window.location.href.length - 10) == "tasks.html") {
-        window.location.href = "index.html";
-    } else {
-        window.location.href = "tasks.html";
-    }
-};
 
 document.getElementById('weather').onclick = () => {
     window.location.href = "https://www.wunderground.com/";
