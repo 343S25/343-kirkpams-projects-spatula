d55f64ad43bad739525a4853b174435fb3f005ed
eshanker <elianashanker@gmail.com>
2025-05-09 05:22:52 -0400

    add import/export buttons to tasks page, add new task button without reload, modify some formatting, break my sleep schedule

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -2,9 +2,49 @@ import { EmojiButton } from 'https://cdn.jsdelivr.net/npm/@joeattardi/emoji-butt
 
 const taskForm = document.getElementById('taskForm');
 const formSubmit = document.getElementById('save');
-const saveDIV = document.getElementById('saveDIV');
+const addButton = document.getElementById('add-btn');
+const taskList = document.getElementById('task-list');
+
+const importButton = document.getElementById('import-btn');
+const exportButton = document.getElementById('export-btn');
 
 const currTasks = localStorage.getItem('tasks');
+
+function addTask() {
+    let currLen = document.querySelectorAll('.task-entry').length + 1;
+
+    const template = document.getElementById('taskField').content.cloneNode(true);
+    // let temp = template.cloneNode(true);
+    let label = template.querySelector('label');
+    let input = template.querySelector('input');
+    let emojiBtn = template.querySelector('.emoji-trigger');
+    let deleteBtn = template.querySelector('.btn-delete');
+
+    label.textContent += " " + currLen;
+    input.id += currLen;
+    // console.log(i - 1);
+    // console.log(tasksArray[i-1].task);
+    try {
+        input.value = tasksArray[currLen].task;
+        emojiBtn.textContent = tasksArray[currLen].emoji || '⭐';
+    } catch (e) {
+        emojiBtn.textContent = '⭐'
+    }
+    // Attach emoji picker and trigger to current input
+    const picker = new EmojiButton();
+    picker.on('emoji', emoji => {
+        emojiBtn.textContent = emoji.emoji;
+    });
+
+    emojiBtn.addEventListener('click', () => picker.togglePicker(emojiBtn));
+    deleteBtn.addEventListener('click', () => {
+        taskList.removeChild(taskList.children[currLen - 1]);
+        saveData();
+        location.reload();
+    });
+
+    taskList.appendChild(template);
+}
 
 function saveData(e) {
     const formData = new FormData(taskForm, formSubmit);
@@ -38,6 +78,32 @@ function saveData(e) {
 
 }
 
+function exportTasks() {
+    saveData();
+    let item = { "tasks": JSON.parse(localStorage.getItem("tasks")) };
+    item = 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(item));
+
+    let dl = document.createElement("a");
+    dl.href = item;
+    dl.download = "spatula-export.json";
+
+    dl.click();
+}
+
+function importTasks() {
+    let file = importButton.files[0];
+
+    const fileReader = new FileReader();
+    fileReader.addEventListener('load', (e) => {
+        let result = JSON.parse(e.target.result).tasks;
+        localStorage.setItem('tasks', JSON.stringify(result));
+        location.reload();
+    });
+    fileReader.readAsText(file);
+}
+
+// stuff from transportodon ends
+
 // If there's at least one item previously saved
 if (currTasks) {
     let tasksArray = JSON.parse(localStorage.getItem('tasks'));
@@ -70,12 +136,12 @@ if (currTasks) {
 
         emojiBtn.addEventListener('click', () => picker.togglePicker(emojiBtn));
         deleteBtn.addEventListener('click', () => {
-            taskForm.removeChild(taskForm.children[i + 1]);
+            taskList.removeChild(taskList.children[i - 1]);
             saveData();
             location.reload();
         });
 
-        taskForm.appendChild(template);
+        taskList.appendChild(template);
     }
 } else {
     const template = document.getElementById('taskField').content.cloneNode(true);
@@ -95,13 +161,17 @@ if (currTasks) {
 
     emojiBtn.addEventListener('click', () => picker.togglePicker(emojiBtn));
     deleteBtn.addEventListener('click', () => {
-        taskForm.removeChild(taskForm.children[i + 1]);
+        taskList.removeChild(taskList.children[i - 1]);
         saveData();
         location.reload();
     });
 
-    taskForm.appendChild(template);
+    taskList.appendChild(template);
 }
 
 
 formSubmit.addEventListener('click', saveData);
+addButton.addEventListener('click', addTask);
+
+importButton.addEventListener('input', importTasks);
+exportButton.addEventListener('click', exportTasks);
